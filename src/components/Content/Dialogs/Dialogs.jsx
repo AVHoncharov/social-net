ac63d03aaacb51af6fee1b8e9ce3a1ebd3efaa26
Alexandr Hancharov <gonzalo.radrigues@gmail.com>
2020-09-21 15:01:21 +0300

    it-cam-22 (Route exact) last

--- src/components/Content/Dialogs/Dialogs.jsx
+++ src/components/Content/Dialogs/Dialogs.jsx
@@ -6,14 +6,24 @@ const Dialogs = (props) => {
   return (
     <div className={style.dialogs}>
       <div className={style.dialogsItems}>
-        <div className={style.dialog + ' ' + style.active}>
-            <NavLink to="/dialogs/1">One</NavLink>
+        <div className={style.dialog + " " + style.active}>
+          <NavLink to="/dialogs/1">One</NavLink>
         </div>
-        <div className={style.dialog}><NavLink to="/dialogs/2">Two</NavLink></div>
-        <div className={style.dialog}><NavLink to="/dialogs/3">Three</NavLink></div>
-        <div className={style.dialog}><NavLink to="/dialogs/4">Four</NavLink></div>
-        <div className={style.dialog}><NavLink to="/dialogs/5">Five</NavLink></div>
-        <div className={style.dialog}><NavLink to="/dialogs/6">Six</NavLink></div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/2">Two</NavLink>
+        </div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/3">Three</NavLink>
+        </div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/4">Four</NavLink>
+        </div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/5">Five</NavLink>
+        </div>
+        <div className={style.dialog}>
+          <NavLink to="/dialogs/6">Six</NavLink>
+        </div>
       </div>
       <div className={style.messages}>
         <div className={style.message}>Message 1</div>
