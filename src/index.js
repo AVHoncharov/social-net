33a62363fb150ff89f6fe08d7909e8ade983d805
Alexandr Hancharov <gonzalo.radrigues@gmail.com>
2021-01-28 11:46:13 +0300

    42. Redux 	- installed redux; 	- made redux-store instead store.js 	- in index.js refactored 	 store.subscribe(() => {     	  let state = store.getState()     	  renderEntireTree(state) 	 });

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
-import store from "./redux/state"
+import store from "./redux/redux-store"
+
 
 const renderEntireTree = (state) => {
     ReactDOM.render(
@@ -18,7 +19,10 @@ const renderEntireTree = (state) => {
 }
 renderEntireTree(store.getState());
 
-store.subscribe(renderEntireTree);
+store.subscribe(() => {
+    let state = store.getState()
+    renderEntireTree(state)
+});
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -1,7 +1,26 @@
 const ADD_MESSAGE = 'ADD-MESSAGE';
 const UPDATE_NEW_MESSAGE_TEXT = 'UPDATE-NEW-MESSAGE-TEXT';
 
-const dialogsReducer = (state, action) => {
+let initialState = {
+  users: [
+    { id: 1, name: "One" },
+    { id: 2, name: "Two" },
+    { id: 3, name: "Three" },
+    { id: 4, name: "Four" },
+    { id: 5, name: "Five" },
+    { id: 6, name: "Six" },
+  ],
+
+  messagesData: {
+    messages: [
+      { id: 1, message: "message 1" },
+      { id: 2, message: "message 2" },
+      { id: 3, message: "message 3" },
+    ],
+    newMessage: "",
+  },
+};
+const dialogsReducer = (state = initialState, action) => {
     switch(action.type) {
         case ADD_MESSAGE: {
             let newMessage = {
