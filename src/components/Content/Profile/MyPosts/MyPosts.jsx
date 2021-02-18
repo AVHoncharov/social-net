cc58f873b5c3c4b0dbfa4e784fd40826321f7eda
AVHoncharov <AVHoncharow@gmail.com>
2021-02-18 09:56:20 +0300

    87. shouldComponentUpdate, React.memo - use React.memo, or shouldComponentUpdate/React.PureComponent if we want   to pretend rerender component in case it's props or state haven't been chaged
    shouldComponentUpdate (должна компонента обновиться) - это метод жизненного цикла. React вызывает этот метод у компоненты, чтобы спросить компоненту: нужно ли вызывать повторно render, получу ли я, React, от тебя новый JSX. Для этого я дам тебе, компонента, новые пропсы и стейт (а может и не новые, а те же самые, что и были ранее) и ты на основе логики своей внутренней верни мне из этого метода true или false... Если вернешь false, то я не буду вызывать render
    
    Есть много деталей. Можно сделать поверхностное сравнение объектов и понять,
     изменились ли данные на входе компонент (props + state), что повлияет на
     выход (JSX).
    
    Такую простую проверку в этом же методе делает компонент PureComponent.
     И если мы отнаследуем нашу классовую компоненту не от React.Component,
     а от React.PureComponent, то мы получим реализованный за нас метод
     shouldComponentUpdate.
    
    Это касается классовых компонент. А как нам автоматизировать перерисовку
     (не вызывать перерисовку, если это не нужно) для функциональных компонент?
     Для этого есть HOC (High Order Component) React.memo, на вход которого мы
    подаём компонент, который хотим оптимизировать, а на выходе получаем контейнерную
     компоненту, которая займётся этой проверкой и перерисовкой

--- src/components/Content/Profile/MyPosts/MyPosts.jsx
+++ src/components/Content/Profile/MyPosts/MyPosts.jsx
@@ -2,30 +2,34 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 import style from "./MyPosts.module.css";
 import Post from "./Post/Post";
-import { maxLengthCreator, required } from './../../../../utils/validators/validators';
-import { Element } from './../../../common/FormsControls/FormsControls';
+import {
+  maxLengthCreator,
+  required,
+} from "./../../../../utils/validators/validators";
+import { Element } from "./../../../common/FormsControls/FormsControls";
 
-const MyPosts = (props) => {
+const MyPosts = React.memo((props) => {
   let postsElements = props.profilePage.posts.map((p) => (
     <Post id={p.id} message={p.message} likeCount={p.likeCount} />
   ));
 
   let onAddNewPost = (data) => {
     props.addPost(data.newPostText);
-  }
+  };
 
   return (
     <div className={style.postsBlock}>
       <div>
         <h3>My posts</h3>
-        <ReduxAddNewPostForm onSubmit={onAddNewPost}/>
+        <ReduxAddNewPostForm onSubmit={onAddNewPost} />
         <div className={style.posts}>{postsElements}</div>
       </div>
     </div>
   );
-};
+});
+
 const maxLength10 = maxLengthCreator(10);
-const Textarea = Element('textarea');
+const Textarea = Element("textarea");
 
 const AddNewPostForm = (props) => {
   return (
@@ -43,6 +47,8 @@ const AddNewPostForm = (props) => {
   );
 };
 
-const ReduxAddNewPostForm = reduxForm({form:'newPostTextForm'})(AddNewPostForm);
+const ReduxAddNewPostForm = reduxForm({ form: "newPostTextForm" })(
+  AddNewPostForm
+);
 
 export default MyPosts;
