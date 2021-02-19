4038bf16b899b04c8cee48ab9dc911c3d486405a
AVHoncharov <AVHoncharow@gmail.com>
2021-02-19 09:20:29 +0300

    93. refactored paginator page

--- src/components/common/Paginator/Paginator.jsx
+++ src/components/common/Paginator/Paginator.jsx
@@ -1,33 +1,88 @@
-import React from "react";
-import style from './Paginator.module.css'
+import React, { useState } from "react";
+import style from "./Paginator.module.css";
 
-const Paginator = (props) => {
-  let pagesCount = Math.ceil(props.totalCount / props.pageSize);
+const Paginator = ({
+  totalCount,
+  pageSize,
+  currentPage,
+  onPageChanged,
+  portionSize = 10,
+}) => {
+  let pagesCount = Math.ceil(totalCount / pageSize);
   let pagesList = [];
   for (let i = 1; i < pagesCount; i++) {
     pagesList.push(i);
   }
 
+  const portionCount = Math.ceil(pagesCount / portionSize);
+  const [portionNumber, setPortionNumber] = useState(1);
+
+  const leftPortionPageNumber = (portionNumber - 1) * portionSize + 1;
+  const rightPortionPageNumber = portionNumber * portionSize;
+
   return (
-      <div>
-        {pagesList.map((page) => {
+    <div className={style.paginatorLine}>
+      {portionNumber > 1 && (
+        <span>
+          <button
+            onClick={() => {
+              setPortionNumber(1);
+            }}
+          >
+            &lt;&lt;
+          </button>
+
+          <button
+            onClick={() => {
+              setPortionNumber(portionNumber - 1);
+            }}
+          >
+            &lt;
+          </button>
+        </span>
+      )}
+
+      {pagesList
+        .filter(
+          (page) =>
+            page >= leftPortionPageNumber && page <= rightPortionPageNumber
+        )
+        .map((page) => {
           return (
             <span
               key={page}
               className={
-                props.currentPage === page
-                  ? style.selectedPage
-                  : style.defaultPage
+                currentPage === page ? style.selectedPage : style.defaultPage
               }
-              onClick={(e) =>{ 
-                props.onPageChanged(page)}
-              }
+              onClick={(e) => {
+                onPageChanged(page);
+              }}
             >
               {page}
             </span>
           );
         })}
-      </div>
+      {portionCount > portionNumber && (
+        <span>
+        <button
+          onClick={() => {
+            setPortionNumber(portionCount);
+          }}
+        >
+         &gt;&gt;
+        </button>
+
+        <button
+          onClick={() => {
+            setPortionNumber(portionNumber + 1);
+          }}
+        >
+          &gt;
+        </button>
+      </span>
+
+      )}
+    </div>
   );
 };
 
